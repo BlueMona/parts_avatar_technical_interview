f740d0007454dbedb4f90ad0d9ded89564f15277
Mona <monagz@gmail.com>
2026-09-11 12:52:32 -0400

    Chore: add try/except for KeyError

--- python_tasks/task_5_nested.py
+++ python_tasks/task_5_nested.py
@@ -3,9 +3,16 @@
 
 def get_vehicle_year(data):
     # TODO: Write your logic here safely
-    return data["specs"]["model_info"]["year"]
+    try:
+        return data["specs"]["model_info"]["year"]
+    except KeyError:
+        return "Unknown"
 
 # Test Case
 vehicle = {'specs': {'model_info': {'year': 2024}}}
 # Expected: 2024
 print(get_vehicle_year(vehicle))
+print(get_vehicle_year({}))
+print(get_vehicle_year({'specs': {}}))
+print(get_vehicle_year({'specs': {'model_info': {}}}))
+print(get_vehicle_year({'specs': {'model_info': {'name' : 'Camry'}}}))
