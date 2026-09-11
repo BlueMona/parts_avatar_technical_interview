1f5cab7a8686976a3bc286028d81aff16b5a3868
Mona <monagz@gmail.com>
2026-09-11 12:31:49 -0400

    Chore: handle string conversion

--- python_tasks/task_3_errors.py
+++ python_tasks/task_3_errors.py
@@ -7,8 +7,11 @@ def calculate_discount(price, discount_percent):
     or if inputs are strings/None. Return 0 for invalid inputs.
     """
     # TODO: Implement logic
+    discount_amount = float(price)*float(discount_percent)*(0.01)
+    return discount_amount
     
 
 # Test Case
 print(calculate_discount(100, "10")) # Should return 0 or handle conversion
 print(calculate_discount(100, 0))    # Should return 0
+print(calculate_discount(None, 10))
