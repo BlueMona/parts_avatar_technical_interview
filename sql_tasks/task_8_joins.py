155029850d22f15a4496cc7d15ae952d0bf9a597
Mona <monagz@gmail.com>
2026-09-18 14:15:50 -0400

    Chore: initial attempt at join query with printed output

--- sql_tasks/task_8_joins.py
+++ sql_tasks/task_8_joins.py
@@ -1,16 +1,32 @@
 import sqlite3
 
 def get_customer_spend():
+
+    #print ("I am in the get customer spend function")
     conn = sqlite3.connect('parts_avatar.db')
     cursor = conn.cursor()
     
     # Task: Join Customers, Orders, and Order_Items to calculate 
     # total spend (price * quantity) per Customer Name.
-    query = """
-    -- WRITE YOUR SQL HERE
+    query ="""
+    SELECT i.price * i.quantity 
+    FROM Customers c 
+    JOIN Orders o
+    JOIN Order_Items i 
+    WHERE c.customer_id = o.customer_id 
     """
     
     cursor.execute(query)
+
+    for row in cursor: 
+        print(row)
+
+
     results = cursor.fetchall()
+
+    
     conn.close()
     return results
+
+if __name__ == "__main__":
+    get_customer_spend()
